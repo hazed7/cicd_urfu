cc4b06b3e13ffce8f0c65f96b4041d3c2250ed6f
Alexander Kostin <iheartapplejr@gmail.com>
2025-12-07 18:21:04 +0300

    feat(api): add flask calc

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from 3!")
+from typing import Callable
+
+from flask import Flask, jsonify, request
+
+from src.calc import Calculator
+
+app = Flask(__name__)
+calculator = Calculator()
+
+def parse_nums() -> tuple[float, float] | tuple[None, None]:
+    lhs_str: str = request.args.get("lhs", "")
+    rhs_str: str = request.args.get("rhs", "")
+
+    try:
+        lhs = float(lhs_str)
+        rhs = float(rhs_str)
+        return lhs, rhs
+    except (ValueError, TypeError):
+        return None, None
+
+def handle(operation: Callable[[float, float], float]):
+    lhs, rhs = parse_nums()
+    if lhs is None or rhs is None:
+        return jsonify(error="Invalid input"), 400
+
+    try:
+        result = operation(lhs, rhs)
+        return jsonify(result=result)
+    except ZeroDivisionError:
+        return jsonify(error="Cannot divide by zero"), 400
+
+
+@app.get("/add")
+def add():
+    return handle(calculator.add)
+
+@app.get("/subtract")
+def subtract():
+    return handle(calculator.subtract)
+
+@app.get("/multiply")
+def multiply():
+    return handle(calculator.multiply)
+
+@app.get("/divide")
+def divide():
+    return handle(calculator.divide)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
---
+++ src/calc.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Calculator:
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        return a - b
+
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        if b == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
+        return a / b
